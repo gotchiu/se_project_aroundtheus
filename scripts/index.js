5e1980a665d095cc85aeaba7e8106210f2440a76
Tiffany Chiu <gotchiu@gmail.com>
2023-09-19 01:11:35 +0800

    update card template

--- scripts/index.js
+++ scripts/index.js
@@ -25,7 +25,9 @@ const initialCards = [
   },
 ];
 
-/* ELEMENTS */
+/* -------------------------------------------------------------------------- */
+/*                                  ELEMENTS                                  */
+/* -------------------------------------------------------------------------- */
 
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
@@ -39,14 +41,53 @@ const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
 const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListElement = document.querySelector('.cards__list')
 
-/* FUNCTIONS */
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+
+/* -------------------------------------------------------------------------- */
+/*                                  FUNCTIONS                                 */
+/* -------------------------------------------------------------------------- */
 
 function closePopup() {
   profileEditModal.classList.remove("modal_opened");
 }
 
-/* EVENT LISTENERS */
+function getCardElement(cardData) {
+     const cardElement = cardTemplate.cloneNode(true);
+  const cardImageElement = cardElement.querySelector('.card__image');
+    const cardTitleElement = cardElement.querySelector('.card__title');
+
+cardImageElement.content = cardData.link;
+
+cardTitleElement.textContent = cardData.name;
+return cardElement;
+
+
+set the path to the image to the link field of the object
+set the image alt text to the name field of the object
+
+
+
+}
+
+
+/* -------------------------------------------------------------------------- */
+/*                               EVENT HANDLERS                               */
+/* -------------------------------------------------------------------------- */
+
+function handleProfileEditSubmit(event) {
+  event.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
+}
+ 
+
+/* -------------------------------------------------------------------------- */
+/*                               EVENT LISTENERS                              */
+/* -------------------------------------------------------------------------- */
 
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
@@ -54,13 +95,14 @@ profileEditButton.addEventListener("click", () => {
   profileEditModal.classList.add("modal_opened");
 });
 
-profileEditCloseButton.addEventListener("click", () => {
-  closePopup();
-});
+profileEditCloseButton.addEventListener("click", closePopup);
 
-profileEditForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  profileTitle.textContent = profileTitleInput.value;
-  profileDescription.textContent = profileDescriptionInput.value;
-  closePopup();
-});
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach(cardData) => {
+const cardElement = getCardElement(cardData); 
+cardListElement.prepend(cardElement);
+};
+
+
+
