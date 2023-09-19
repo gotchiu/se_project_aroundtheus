16607c6bd1b0562781ce849d2a1f12312d36c514
Tiffany Chiu <gotchiu@gmail.com>
2023-09-19 13:52:41 +0800

    update card template images

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
 const initialCards = [
   {
     name: "Yosemite Valley",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+    link: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8eW9zZW1pdGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
   },
   {
     name: "Lake Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+    link: "https://images.unsplash.com/photo-1539667547529-84c607280d20?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bGFrZSUyMGxvdWlzZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
   },
   {
     name: "Bald Mountains",
@@ -13,7 +13,7 @@ const initialCards = [
   },
   {
     name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+    link: "https://images.unsplash.com/photo-1598332504664-4717290189e5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80",
   },
   {
     name: "Vanoise National Park",
@@ -41,7 +41,7 @@ const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
 const profileEditForm = profileEditModal.querySelector(".modal__form");
-const cardListElement = document.querySelector('.cards__list')
+const cardListElement = document.querySelector(".cards__list");
 
 const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
@@ -55,23 +55,16 @@ function closePopup() {
 }
 
 function getCardElement(cardData) {
-     const cardElement = cardTemplate.cloneNode(true);
-  const cardImageElement = cardElement.querySelector('.card__image');
-    const cardTitleElement = cardElement.querySelector('.card__title');
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageElement = cardElement.querySelector(".card__image");
+  const cardTitleElement = cardElement.querySelector(".card__title");
 
-cardImageElement.content = cardData.link;
+  cardImageElement.src = cardData.link;
+  cardImageElement.alt = cardData.name;
 
-cardTitleElement.textContent = cardData.name;
-return cardElement;
-
-
-set the path to the image to the link field of the object
-set the image alt text to the name field of the object
-
-
-
+  cardTitleElement.textContent = cardData.name;
+  return cardElement;
 }
-
 
 /* -------------------------------------------------------------------------- */
 /*                               EVENT HANDLERS                               */
@@ -83,7 +76,6 @@ function handleProfileEditSubmit(event) {
   profileDescription.textContent = profileDescriptionInput.value;
   closePopup();
 }
- 
 
 /* -------------------------------------------------------------------------- */
 /*                               EVENT LISTENERS                              */
@@ -99,10 +91,7 @@ profileEditCloseButton.addEventListener("click", closePopup);
 
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
 
-initialCards.forEach(cardData) => {
-const cardElement = getCardElement(cardData); 
-cardListElement.prepend(cardElement);
-};
-
-
-
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListElement.prepend(cardElement);
+});
